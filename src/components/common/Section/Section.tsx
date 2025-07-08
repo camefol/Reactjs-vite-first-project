87c6f56fdff87acc6776a41cca31673b37bcd7a2
camefol <camefol.janecko38@gmail.com>
2025-07-08 13:17:44 +0200

    #

--- src/components/common/Section/Section.tsx
+++ src/components/common/Section/Section.tsx
@@ -12,7 +12,7 @@ interface LayoutProps {
 
 const variantClasses: Record<Variant, string> = {
   default: '',
-  dark: 'bg-gradient-to-r from-gray-200 via-gray-100 to-gray-150',
+  dark: 'bg-gradient-to-r from-gray-200 via-gray-100 to-gray-200',
   blue:'bg-gradient-to-r from-blue-400 via-indigo-300 to-blue-500'
 }
 
