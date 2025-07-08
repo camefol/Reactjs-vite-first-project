f182e019ec472f148edda2695730127b6f34121f
camefol <camefol.janecko38@gmail.com>
2025-07-08 12:58:35 +0200

    #Hello

--- src/components/common/Navigator/Navigator.tsx
+++ src/components/common/Navigator/Navigator.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { NavLink } from 'react-router-dom'
 
 const Navigator = () => {
   const [activeItem, setActiveItem] = useState('Home')
@@ -9,9 +10,6 @@ const Navigator = () => {
     {id:'profile', label:'Profile', path:'/profile'},
   ]
 
-  const handleItemClick = (label) => {
-    setActiveItem(label)
-  }
 
   return (
     <nav className="w-full bg-white shadow-sm">
@@ -19,18 +17,18 @@ const Navigator = () => {
         <ul className="flex justify-center space-x-1 sm:space-x-4 py-2 sm:py-4">
         {menuItems.map(item => (
             <li key={item.id} className="flex-1 sm:flex-none">
-              <button
-                onClick={() => handleItemClick(item.label)}
-                className={`
-                  block w-full px-2 sm:px-4 py-2 rounded-md text-sm sm:text-lg md:text-xl font-medium transition-all duration-300 text-center
-                  ${activeItem === item.label
-                    ? 'bg-blue-500 text-white shadow-md' 
+               <NavLink 
+                to={item.path}
+                className={({ isActive }) => `
+                  px-3 py-2 mx-10 rounded-md text-2xl font-medium transition-all duration-500 animate-pulse
+                  ${isActive 
+                    ? 'bg-blue-300 text-white scale-150' 
                     : 'text-gray-700 hover:text-blue-600 hover:bg-gray-100'
                   }
                 `}
               >
                 {item.label}
-              </button>
+              </NavLink>
             </li>
           ))}
         </ul>
